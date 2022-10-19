f27954ccb301715abd715ee43a7cd63ab030d5b1
Mussesan <gmusse.dev@gmail.com>
2022-10-18 23:56:59 -0300

    metodo registro com localStorage, adicionado

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticateService } from './authenticate.service';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  
+
+  constructor(private auth: AuthenticateService) { }
+
+  private user: User;
 
   ngOnInit(): void {
+  }
+
+  fazerLogin(){
+    
   }
 
 }
